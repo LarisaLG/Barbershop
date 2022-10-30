3348ef9137892fd25d86cf58d8e368864715bf82
LarisaLG <lgalisheva@gmail.com>
2022-10-30 11:24:27 +0000

    docs: create Services and Booking models, regeister in admin.py

--- barbershop/models.py
+++ barbershop/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from enum import unique
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+
+class Service(models.Model):
+    """The model is used to display data about the services
+    provided by the barbershop
+    """
+    service_name = models.CharField(max_length=150, unique=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        unique_together = ('service_name', 'price', )
+        ordering = ['-price']
+
+    def __str__(self):
+        return f"{self.service_name}, {self.price}"
+
+
+HOURS = (
+    ('10:00', '10:00'),
+    ('10:30', '10:30'),
+    ('11:00', '11:00'),
+    ('11:30', '12:30'),
+    ('12:00', '12:00'),
+    ('12:30', '12:30'),
+    ('13:00', '13:00'),
+    ('13:30', '13:30'),
+    ('14:00', '14:00'),
+    ('14:30', '14:30'),
+    ('15:00', '15:00'),
+    ('15:30', '15:30'),
+    ('16:00', '16:00'),
+    ('16:30', '16:30'),
+    ('17:00', '17:00'),
+    ('17:30', '17:30'),
+    ('18:00', '18:00'),
+    ('18:30', '18:30'),
+    ('19:00', '19:00'),
+)
+
+
+class Booking(models.Model):
+
+    """Model that is used to store the data that the user enters in the
+    booking form
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
+    # phone = PhoneNumberField(unique=True, null=False, blank=False)
+    phoneRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    phone = models.CharField(validators=[phoneRegex], max_length=16, unique=True, null=False, blank=False)
+    date = models.DateField()
+    time = models.CharField(max_length=30, choices=HOURS, default='10:00')
+
+    class Meta:
+        unique_together = ('user', 'phone', 'service')
+
+    def __str__(self):
+        return self.name
