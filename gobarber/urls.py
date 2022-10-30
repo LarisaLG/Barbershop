c016bd4296ebd7a6acde00088045e05b49b008cc
LarisaLG <lgalisheva@gmail.com>
2022-10-30 20:06:37 +0000

    docs: setup allauth for account registration

--- gobarber/urls.py
+++ gobarber/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('barbershop.urls'))
+    path('', include('barbershop.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
