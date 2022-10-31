0627ea4ef7eb0c4696a0a2d36615531762698e6e
LarisaLG <lgalisheva@gmail.com>
2022-10-31 16:51:47 +0000

    docs: create booknow.html template, link in urls.py

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('services/', views.services, name="services"),
+    path('booknow/', views.booknow, name='booknow'),
 ]
